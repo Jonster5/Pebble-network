0c1dd0c1abddce06b864fd0b266e90f9ce44c785
Jonster5 <49014692+Jonster5@users.noreply.github.com>
2019-12-02 17:55:13 -0500

    indev-0

--- index.js
+++ index.js
@@ -8,18 +8,32 @@ let wsServer = new webSocketServer({
     httpServer: server
 });
 
+let clients = [];
+
 wsServer.on('request', req => {
     let connection = req.accept(null, req.origin);
-    console.log(`socket connected ${req.key}`);
+    clients.push({
+        connection: connection,
+        clientID: req.key
+    });
+    console.log(`client connected: ${clients[clients.length - 1].clientID}`);
     connection.on('message', message => {
         if (message.type === 'utf8') {
-            console.log(message.utf8Data);
-            console.log(req.eventNames())
-            connection.send("hello there!");
+            clients.forEach(client => {
+                client.connection.send(message.utf8Data);
+            });
+            console.log(clients.length);
         }
     });
 
     connection.on('close', connection => {
         console.log(`socket closed ${req.key}`);
+
+        clients.splice(clients.length - 1, 1);
+        if (clients > 0) {
+            for (const client of clients) {
+                console.log(client.clientID);
+            }
+        }
     });
 });
