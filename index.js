b67cd4301576a3f1cffceebbee87423394efcb63
Jonster5 <49014692+Jonster5@users.noreply.github.com>
2019-12-02 21:11:29 -0500

    indev-1

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
-const webSocketServer = require('websocket').server;
-const express = require('express');
-let app = express();
-let server = app.listen(1337);
-app.use(express.static('public'));
+/** Class for creating servers, serving static pages, and controlling websockets */
+exports.Pebble = {
+    /**
+     * Creates a new HTTP server and serves a static folder
+     * @param {string} static_url 
+     * @param {number} port 
+     */
+    initServer: function(static_url = "", port = 1337) {
+        this.staticURL = static_url;
+        this.port = 1337;
 
-let wsServer = new webSocketServer({
-    httpServer: server
-});
+        this._express = require('express');
+        let app = this._express();
+        let serv = app.listen(this.port);
+        app.use(this._express.static(this.staticURL));
+        this.server = serv;
+    },
+    initSockets: function() {
+        if (this.server === undefined) throw new Error('call Pebble.initServer(...) before using this function');
+        this.clients = [];
+        this.wsServer = new require('websocket').server({
+            httpServer: this.server
+        });
+    },
+    request: function(request) {
+        if (this.wsServer === undefined) throw new Error('call Pebble.initServer(...) before using this function');
+        this.wsServer.on('request', req => {
+            this.clients.push({
+                connection: req.accept(null, req.origin),
+                clientID: req.key,
+            });
+        });
+    }
+}
 
 let clients = [];
 
@@ -17,6 +42,7 @@ wsServer.on('request', req => {
         clientID: req.key
     });
     console.log(`client connected: ${clients[clients.length - 1].clientID}`);
+    console.log(clients.length);
     connection.on('message', message => {
         if (message.type === 'utf8') {
             clients.forEach(client => {
